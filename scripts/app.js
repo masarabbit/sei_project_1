54b8b51b1e0c03bf5b8af762f0cad4120881f895
Masa Takeuchi <masahitoart@gmail.com>
2020-11-24 14:25:17 +0000

    second item added

--- scripts/app.js
+++ scripts/app.js
@@ -5,28 +5,52 @@
 
 //! issue of points getting blurry? reason unknown.
 
-
-
-
 function init() {
+
+
+  
   
   //*items and wall positions
   const cellsWithWalls = [27,66,68,106,108,126,127,128,129,130,131,132,133,134,135,138,139,140,141,142,143,144,145,146,148,149,150,151,152,153,154,155,165,175,178,196,205,207,208,210,211,212,213,215,218,220,221,222,223,224,225,226,228,229,230,231,232,233,234,236,245,247,248,250,253,255,258,260,266,268,274,276,285,287,288,290,293,295,298,300,306,308,314,316,324,325,327,328,330,331,332,333,335,336,337,338,340,341,342,343,344,345,346,348,349,350,351,352,354,356,363,392,394,396,403,405,406,407,408,409,410,411,413,414,416,417,418,419,420,421,422,424,425,428,429,430,432,433,434,436,443,445,451,453,454,456,457,458,459,460,461,462,464,465,468,470,476,483,485,491,493,494,499,500,508,510,512,513,514,515,516,523,525,526,527,528,529,530,531,533,534,536,537,539,540,542,543,544,546,547,548,550,552,553,554,555,556,563,570,571,573,574,576,577,579,580,582,583,584,586,587,588,590,596,604,605,606,607,608,610,611,628,630,631,632,633,634,636,648,650,651,652,653,655,656,664,665,668,674,676,681,682,683,684,685,686,687,688,690,691,692,693,695,696,704,705,708,709,710,711,712,713,714,716,717,718,720,735,736,744,745,759,761,762,763,765,766,767,768,769,770,771,772,773,775,776,777,778,779,780,781,782,783,784,785,788,789,790,791,793,794,795,796,797,798,803,805,806,807,808,809,810,811,812,813,815,816,817,818,819,820,821,822,823,824,825,828,831,833,843,845,846,868,869,870,871,873,874,875,876,883,885,886,888,889,890,891,893,894,896,897,898,899,900,902,903,905,906,908,909,916,923,925,926,928,929,930,931,933,934,936,937,938,939,940,942,943,945,946,948,949,951,952,953,954,956,963,973,974,979,980,982,983,985,986,988,989,991,994,996,1003,1005,1006,1007,1008,1009,1011,1012,1013,1014,1015,1016,1017,1019,1020,1022,1023,1025,1026,1028,1029,1031,1034,1036,1043,1045,1046,1047,1048,1049,1051,1052,1053,1054,1055,1056,1057,1059,1060,1065,1066,1068,1069,1071,1074,1076,1083,1085,1086,1099,1100,1101,1102,1103,1104,1105,1106,1108,1109,1111,1112,1113,1114,1116,1123,1125,1126,1128,1129,1131,1132,1134,1135,1136,1137,1139,1140,1141,1142,1143,1144,1145,1146,1156,1163,1168,1169,1171,1172,1174,1177,1185,1186,1187,1188,1190,1191,1193,1194,1195,1196,1203,1205,1206,1207,1208,1209,1211,1212,1214,1217,1218,1219,1221,1222,1223,1225,1226,1227,1228,1230,1231,1233,1234,1235,1236,1243,1245,1246,1247,1248,1249,1251,1252,1254,1259,1261,1263,1276,1283,1285,1286,1291,1292,1294,1295,1296,1297,1298,1299,1301,1303,1304,1305,1307,1308,1310,1311,1313,1314,1316,1323,1325,1326,1328,1329,1331,1332,1338,1339,1341,1345,1347,1348,1350,1351,1353,1354,1356,1363,1365,1366,1368,1369,1371,1372,1373,1374,1375,1376,1378,1379,1381,1382,1383,1384,1385,1387,1388,1390,1391,1393,1394,1396,1403,1411,1412,1413,1414,1415,1416,1430,1431,1436,1444,1445,1446,1447,1448,1449,1450,1451,1452,1453,1454,1455,1456,1457,1458,1459,1460,1461,1462,1463,1464,1465,1466,1468,1469,1470,1471,1472,1473,1474,1475,1506,1508,1546,1548,1587]
 
-  
-
-  const cellsWithItems = [167,168,169,170,171,172,173,174,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,206,209,214,219,227,235,246,249,254,259,267,275,286,289,294,299,307,315,326,329,334,339,347,355,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386,387,388,389,390,391,395,404,412,415,423,426,427,431,435,444,452,455,463,466,467,471,472,473,474,475,484,492,495,496,497,498,501,502,503,504,505,506,507,511,524,532,535,538,541,545,551,564,565,566,567,568,569,572,575,578,581,585,591,592,593,594,595,609,612,613,614,615,616,617,618,619,620,621,622,623,624,625,626,627,635,649,654,666,667,675,689,694,706,707,715,724,725,726,727,728,730,731,732,733,734,746,747,748,749,750,751,752,753,754,755,764,774,786,787,792,804,814,826,827,832,844,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,872,884,887,892,895,901,904,907,910,911,912,913,914,915,924,927,932,935,941,944,947,950,955,964,965,966,967,968,969,970,971,972,975,976,977,978,981,984,987,990,995,1004,1010,1018,1021,1024,1027,1030,1035,1044,1050,1058,1061,1062,1063,1064,1067,1070,1075,1084,1087,1088,1089,1090,1091,1092,1093,1094,1095,1096,1097,1098,1107,1110,1115,1124,1127,1130,1133,1138,1147,1148,1149,1150,1151,1152,1153,1154,1155,1164,1165,1166,1167,1170,1173,1178,1179,1180,1181,1182,1183,1184,1189,1192,1204,1210,1213,1220,1224,1229,1232,1244,1250,1253,1260,1264,1265,1266,1267,1268,1269,1270,1271,1272,1273,1274,1275,1284,1287,1288,1289,1290,1293,1300,1306,1309,1312,1315,1324,1327,1330,1333,1334,1335,1336,1337,1340,1346,1349,1352,1355,1364,1367,1370,1377,1380,1386,1389,1392,1395,1405,1406,1407,1408,1409,1410,1417,1418,1419,1420,1421,1422,1423,1424,1425,1426,1427,1428,1429,1432,1433,1434]
+  // const cellsWithItems = [167,168,169,170,171,172,173,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,206,209,214,219,227,235,246,249,254,259,267,275,286,289,294,299,307,315,326,329,334,339,347,355,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386,387,388,389,390,391,395,404,412,415,423,426,427,431,435,444,452,455,463,466,467,471,472,473,474,475,484,492,495,496,497,501,502,504,505,506,507,511,524,532,535,538,541,545,551,564,565,566,567,568,569,572,575,578,581,585,591,592,593,594,595,609,612,613,614,615,616,617,618,619,620,621,622,623,624,625,626,627,635,649,654,666,667,675,689,694,706,707,715,724,725,726,727,728,730,731,732,733,734,746,747,748,749,750,751,752,753,754,755,764,774,786,787,792,804,814,826,827,832,844,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,872,884,887,892,895,901,904,907,910,911,912,913,914,924,927,932,935,941,944,947,950,955,964,965,966,967,968,969,970,971,972,975,976,977,981,984,987,990,995,1004,1010,1018,1021,1024,1027,1030,1035,1044,1050,1058,1061,1062,1063,1064,1067,1070,1075,1084,1087,1088,1089,1090,1091,1092,1093,1094,1095,1096,1097,1098,1107,1110,1115,1124,1127,1130,1133,1138,1147,1148,1149,1150,1151,1152,1153,1154,1164,1165,1166,1167,1170,1173,1178,1179,1180,1181,1182,1183,1184,1189,1192,1204,1210,1213,1220,1224,1229,1232,1244,1250,1253,1260,1264,1265,1266,1267,1268,1269,1270,1271,1272,1273,1274,1275,1284,1288,1289,1290,1293,1300,1306,1309,1312,1315,1324,1327,1330,1334,1335,1336,1337,1340,1346,1349,1352,1355,1364,1367,1370,1377,1380,1386,1389,1392,1395,1405,1406,1407,1408,1409,1410,1417,1418,1419,1420,1421,1422,1423,1424,1425,1426,1427,1428,1429,1432,1433,1434]
 
   const cellsWithTeleport = [67,721,758,1547]
   const cellsWithTeleportExit = [1507,757,722,107]
   //! this is exact opposite of cells with Teleport, so may be redundant
   
-  const cellsWithBigStars = [166,195,1404,1435]
-  
 
-  //! provisional settings
-  // const cellsWithItems = [167,168,169,170,171,172,173,174]
-  // const cellsWithBigStars = [166]
+  //* items
+
+  const itemObjects = [
+    {
+      itemName: 'normal star',
+      score: 100,
+      class: 'item',
+      image: 'item',
+      additionalEffect: 'nothing',
+      cellsWithItem: [167,168,169,170]
+    },
+    {
+      itemName: 'big star',
+      score: 1000,
+      class: 'big_star',
+      image: 'big_star',
+      additionalEffect: 'invincibility',
+      cellsWithItem: [166]
+      // [166,195,1404,1435]
+    },
+    {
+      itemName: 'blue star',
+      score: 500,
+      class: 'blue_star',
+      image: 'blue_star',
+      additionalEffect: 'nothing',
+      cellsWithItem: [173,174,179]
+      // [174,179,498,503,847,915,978,1155,1287,1333]
+    }
+  ]
+
 
 
   //* page elements
@@ -44,7 +68,7 @@ function init() {
   let score = 0
   const lifeDisplay = document.querySelector('#life')
   //// const lifeDisplayWrapper = document.querySelector('.life_wrapper')
-
+  
 
   // * Variables
   const width = 40
@@ -56,10 +80,19 @@ function init() {
   const lifeCounters = []
   const defaultMotion = ['right','left','up','down']
   const flickerStateDuration = 3000
-  const itemTotal = cellsWithItems.length + cellsWithBigStars.length
+  let itemTotal = 0
   let knockOutCpuCounter = 1
-  let itemToCollect = itemTotal
+  // let itemCollected = 0
 
+  function setItemTotal(){
+    itemObjects.forEach(item =>{
+      itemTotal += item.cellsWithItem.length
+    })
+  }
+  setItemTotal() //! sets item total based on items in itemObjects
+
+  let itemToCollect = itemTotal  //! ensure this is called after setItemTotal
+  let blueStarCollected = 0
   
   //* actors
   const player = {
@@ -79,19 +112,6 @@ function init() {
     life: 3,
     knockOutAnimationDisplay: null,
   }
-
-  // const cpuOneDefaultTarget = {
-  //   position: 378,
-  //   horizontalPosition: null,
-  //   verticalPosition: null,
-  // }
-
-  // const cpuTwoDefaultTarget = {
-  //   position: 1280,
-  //   horizontalPosition: null,
-  //   verticalPosition: null,
-  // }
-
 
   const cpuObjects = [
     {
@@ -165,6 +185,18 @@ function init() {
 
 
 
+  //TODO cpu control
+  //! somthing could trigger cpu status to change (some kind of timer?)
+  //! needs to ensure this doesn't trigger when game is already complete
+
+  function controlCpuActivation(){
+
+    if (itemToCollect === itemTotal - 20 && !cpuObjects[1].display.classList.contains('hidden')){
+      changeStatusToActive(cpuObjects[1])
+    }
+
+  }
+
   
   //TODO
   // page display
@@ -176,24 +208,7 @@ function init() {
   const itemPositionDisplay = document.querySelector('#item_position')
 
 
-  //* SETTINGS
 
-  const itemObjects = [
-    {
-      itemName: 'normal star',
-      score: 100,
-      class: 'item',
-      image: 'item',
-      additionalEffect: 'nothing',
-    },
-    {
-      itemName: 'big star',
-      score: 500,
-      class: 'big_star',
-      image: 'big_star',
-      additionalEffect: 'invincibility'
-    }
-  ]
   
   // * Make a grid 
   function createGrid() {
@@ -230,13 +245,11 @@ function init() {
       })
     },1000)
   
-
     createGrid()
     displayPlayerLife()
     populateCells(cellsWithWalls,'wall')
     populateCells(cellsWithTeleport,'teleport')
-    populateCells(cellsWithItems,'item')
-    populateCells(cellsWithBigStars,'big_star')
+    populateItems()
 
     setTimeout(() =>{
       displayActorImage(player)
@@ -246,29 +259,28 @@ function init() {
     
     setTimeout(() =>{
       player.display.classList.remove('hidden')
-
       initialiseCpus(cpuObjects)  
-    
-      //* checking for status
-      constantCheck = setInterval(() =>{
-        rePositionImage(player) // this is to ensure player image stays in right place despite screen resize
-        checkPlayerAndCpuCollision()
-        triggerTeleport()
-        // rePositionImage(cpuOne) // may not be necessary?(cpu moves anyway)
-      },100)
+      constantCheckAction()
     },2200)
 
+  }
 
-    //TODO to be removed later
-    //! somthing could trigger cpu status to change (some kind of timer?)
-    //! this could be in start game, or somewhere else.
-    //! needs to ensure this doesn't trigger when game is already complete
-    setTimeout(
-      () =>{
-        changeStatusToActive(cpuObjects[1])
-      },5000)
+  function constantCheckAction(){
+    //* checking for status
+    constantCheck = setInterval(() =>{
+      rePositionImage(player) // this is to ensure player image stays in right place despite screen resize
+      checkPlayerAndCpuCollision()
+      controlCpuActivation()
+      triggerTeleport()
+      // rePositionImage(cpuOne) // may not be necessary?(cpu moves anyway)
+    },100)
+  }
 
-
+  function populateItems(){
+    itemObjects.forEach( item =>{
+      populateCells(item.cellsWithItem,item.class)
+    })
+    blueStarCollected = 0
   }
 
   function countDown(){
@@ -316,18 +328,6 @@ function init() {
       cover.removeChild(countDownCircle)
     },2200)
   }
-
-  
-
-  //// function initiateCpu(cpu){
-  ////   displayTargetImage(cpu)
-  ////  setInterval(function(){
-  ////     cpuMovement(cpu)
-  ////   },cpu.speed)
-  ////   addCpu(cpu,cpu.position,cpu.class)
-  //// }
-  
-
 
   //! triggering each cpu's motion could be a separate function?
   function initialiseCpus(cpuObjects){
@@ -583,8 +583,7 @@ function init() {
     player.life = player.defaultLife   // reset player life total
     displayPlayerLife()
 
-    populateCells(cellsWithItems,'item')   // reposition items
-    populateCells(cellsWithBigStars,'big_star')
+    populateItems()
     
     countDown()
 
@@ -601,25 +600,9 @@ function init() {
       displayActorImage(player)
       player.display.classList.remove('hidden')
 
-      constantCheck = setInterval(() =>{
-        rePositionImage(player) // this is to ensure player image stays in right place despite screen resize
-        checkPlayerAndCpuCollision()
-        triggerTeleport()
-      // rePositionImage(cpuOne) // may not be necessary?(cpu moves anyway)
-      },100)
+      constantCheckAction()
 
     },2200)
-
-
-    //TODO to be removed later
-    //! somthing could trigger cpu status to change (some kind of timer?)
-    //! this could be in start game, or somewhere else.
-    //! needs to ensure this doesn't trigger when game is already complete
-    setTimeout(
-      () =>{
-        changeStatusToActive(cpuObjects[1])
-      },5000)
-
 
   }
 
@@ -638,7 +621,7 @@ function init() {
 
 
   function printPosition(){
-    playerPositionDisplay.innerHTML = `${player.position} Horizontal:${player.horizontalPosition} Vertical:${player.verticalPosition} item left:${itemToCollect}`
+    playerPositionDisplay.innerHTML = `${player.position} Horizontal:${player.horizontalPosition} Vertical:${player.verticalPosition} item left:${itemToCollect} blueStar collected:${blueStarCollected}`
     // cpuOnePositionDisplay.innerHTML = `${cpuObjects[0].position} Horizontal:${cpuObjects[0].verticalPosition} Vertical:${cpuObjects[0].verticalPosition} cpu motion: ${cpuObjects[0].motion} cpu facing ${cpuObjects[0].facingDirection}`
     // wallPositionDisplay.innerHTML = `${cellsWithWalls}`
 
@@ -1098,6 +1081,10 @@ function init() {
       turnPlayerInvincible()
     }
 
+    if (outerCells[player.position].classList.contains('blue_star') && !player.display.classList.contains('flicker')){
+      blueStarCollected += 1  //* count blue star collected
+    }
+
     itemObjects.forEach(itemObject =>{
       takeItemAndEarnScore(itemObject) 
     })
@@ -1235,3 +1222,27 @@ window.addEventListener('DOMContentLoaded', init)
   
 //! decide what to include in start game (arguably, elements can be created soon as page is loaded. start game could just enable player to move etc.)
 // startGame()
+
+
+  
+
+//// function initiateCpu(cpu){
+////   displayTargetImage(cpu)
+////  setInterval(function(){
+////     cpuMovement(cpu)
+////   },cpu.speed)
+////   addCpu(cpu,cpu.position,cpu.class)
+//// }
+  
+
+// const cpuOneDefaultTarget = {
+//   position: 378,
+//   horizontalPosition: null,
+//   verticalPosition: null,
+// }
+
+// const cpuTwoDefaultTarget = {
+//   position: 1280,
+//   horizontalPosition: null,
+//   verticalPosition: null,
+// }
